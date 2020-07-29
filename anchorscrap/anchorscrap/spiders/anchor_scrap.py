f1bebe11f95e9c0daf909b02e37c256983ddbbd5
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-07-29 21:31:38 +0530

    feat/scrapping links in the home page

---
+++ anchorscrap/anchorscrap/spiders/anchor_scrap.py
@@ -0,0 +1,31 @@
+import scrapy
+
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # This will select all the anchor tags that are placed immediately
+        # after .author class
+
+        author_page_links = response.css('.author + a')
+        # By default, scrapy filters out duplicated requests to URLs already
+        # visited, avoiding problem of hitting server too much.
+        yield from response.follow_all(author_page_links, self.parse_author)
+
+        pagination_links = response.css('li.next a')
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            # strip() returns a copy of the string by removing both the leading
+            # and trailing characters including \n
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
