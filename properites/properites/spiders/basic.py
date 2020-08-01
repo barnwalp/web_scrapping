679fce8e40a7ff7e8fc77ad237ebb436776d5ed5
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-08-01 17:29:57 +0530

    feat/generating log from within spider

--- properites/properites/spiders/basic.py
+++ properites/properites/spiders/basic.py
@@ -4,7 +4,11 @@ import scrapy
 class BasicSpider(scrapy.Spider):
     name = 'basic'
     allowed_domains = ['web']
-    start_urls = ['http://web/']
+    start_urls = ['http://web:9312/properties/property_000000.html']
 
     def parse(self, response):
-        pass
+        self.log('title: %s' %response.xpath(
+            '//*[@itemprop="name"][1]/text()').extract())
+        self.log('price: %s' %response.xpath(
+            '//*[@itemprop="price"][1]/text()').re('[.0-9]+'))
+
