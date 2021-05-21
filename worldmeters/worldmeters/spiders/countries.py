bee50f46a933943323521a77ac8467151213c97b
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-21 08:02:46 +0530

    feat/getting country year wise population

--- worldmeters/worldmeters/spiders/countries.py
+++ worldmeters/worldmeters/spiders/countries.py
@@ -3,6 +3,7 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -10,7 +11,21 @@ class CountriesSpider(scrapy.Spider):
         for country in countries:
             name = country.css("td a::text").get()
             link = country.css("td a::attr(href)").get()
+            next_link = response.urljoin(link)
+            # passing the name of country usinig meta argument of Request
+            yield scrapy.Request(url=next_link, callback=self.parse_countries, meta={'country_name': name})
+            # # instead of urljoin, one can also use response.follow
+            # yield response.follow(url=link)
+        
+    def parse_countries(self, response):
+        name = response.request.meta['country_name']
+        table = response.css("table.table.table-striped.table-bordered.table-hover.table-condensed.table-list")[0]
+        rows = table.css("tbody tr")
+        for row in rows:
+            year = row.css("td::text").get()
+            population = row.css("td strong::text").get()
             yield{
-                'country_name': name,
-                'country_link': link
+                'name': name,
+                'year': year,
+                'population': population
             }
