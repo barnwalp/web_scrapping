c0d7df356ba77fa18a7e2700dac4553f56a014b3
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-22 06:43:59 +0530

    feat/passing user-agent as header

--- tinydeal/tinydeal/spiders/special_offer.py
+++ tinydeal/tinydeal/spiders/special_offer.py
@@ -4,6 +4,13 @@ import scrapy
 class SpecialOfferSpider(scrapy.Spider):
     name = 'special_offer'
     start_urls = ['https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html']
+    # user-agent can also be added in this way
+    def start_requests(self):
+        yield scrapy.Request(
+            url="https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html",
+            headers={
+                'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+            })
 
     def parse(self, response):
         for product in response.css(".productListing-even"):
@@ -11,10 +18,16 @@ class SpecialOfferSpider(scrapy.Spider):
                 'title': product.css(".p_box_title::text").get(),
                 'url': response.urljoin(product.css(".p_box_title::attr(href)").get()),
                 'discounted_price': product.css(".productSpecialPrice.fl::text").get(),
-                'original_price': product.css(".normalprice.fl::text").get()
+                'original_price': product.css(".normalprice.fl::text").get(),
+                'user-agent': response.request.headers['User-Agent']
             }
 
         next_page = response.css(".nextPage::attr(href)").get()
         if next_page is not None:
             url = response.urljoin(next_page)
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                headers={
+                    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+                })
