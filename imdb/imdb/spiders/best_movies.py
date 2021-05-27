ab3afcab17ce3e4a90cf57f08c74bf4db44cf172
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-27 09:26:30 +0530

    docs/rules and linkextractor object

--- imdb/imdb/spiders/best_movies.py
+++ imdb/imdb/spiders/best_movies.py
@@ -3,13 +3,24 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
+# This spider is inhereting from crawlspider class
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['x']
     start_urls = ['http://x/']
-
+    # rules is tuple; wherein Rule is an object which tells the crawlspider
+    # what are the links you want to follow. In this case you must pass
+    # callback method which must not be parse
+    # LinkExtractor extract all links which contains 'Items' in the url
+    # Instead of allow, you can also pass deny argument which will ensure
+    # that specified urls are not extracted
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        # Rule(LinkExtractor(deny=r'Items/'), callback='parse_item', follow=True),
+        # you can also use xpath or css to extract link. please note that href
+        # argument is not passed as object is capable in extracting links
+        # Rule(LinkExtractor(restrict_xpaths=('//a[@class="active"]')), callback='parse_item', follow=True),
+        # Rule(LinkExtractor(restrict_css=('a.active'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
