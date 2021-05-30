558d80150bfe65da71f51e590a1747fe59a7f823
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-30 11:34:24 +0530

    feat/using regex to modify scrapped data

--- books/books/spiders/book_list.py
+++ books/books/spiders/book_list.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+import re
 
 
 class BookListSpider(CrawlSpider):
@@ -18,11 +19,22 @@ class BookListSpider(CrawlSpider):
     )
 
     def set_user_agent(self, request, spider):
-        request.headers['User-Agnet'] = self.user_agent
+        request.headers['User-Agent'] = self.user_agent
         return request
 
     def parse_item(self, response):
+        yield{
+            'book_name': response.css('h1::text').get(),
+            'book_price': response.css('.price_color::text').get(),
+            'book_review': self.finding_value(response.css('.star-rating::attr(class)').get())
+        }
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
-        print(response.request.headers['User-Agent'])
+        # print(response.request.headers['User-Agent'])
+
+    def finding_value(self, str):
+        pattern = re.compile(r'\s\w+')
+        # findall returns a list with the matched patterns
+        # strip() removes the whitespaces around the word
+        return pattern.findall(str)[0].strip()
