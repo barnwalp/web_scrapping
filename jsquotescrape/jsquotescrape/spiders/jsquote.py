3707afc587eae1b8fc4bcae5f68c5494df9b061e
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-06-01 18:02:58 +0530

    feat/scrapping quotes with pagination

--- jsquotescrape/jsquotescrape/spiders/jsquote.py
+++ jsquotescrape/jsquotescrape/spiders/jsquote.py
@@ -5,9 +5,10 @@ from scrapy.shell import inspect_response
 
 class QuoteSpider(scrapy.Spider):
     name = 'jsquote'
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
 
     script = """
-        function main(splash, args):
+        function main(splash, args)
           assert(splash:go(args.url))
           assert(splash:wait(1))
           return splash:html()
@@ -19,10 +20,30 @@ class QuoteSpider(scrapy.Spider):
             url="http://quotes.toscrape.com/js/",
             callback=self.parse,
             endpoint='execute',
+            headers={
+                'User-Agent': self.user_agent
+            },
             args={
                 'lua_source': self.script
             })
 
     def parse(self, response):
-        print(response.request.headers)
-        inspect_response(response, self)
+        # print(response.request.headers)
+        # inspect_response(response, self)
+        for quotes in response.css('.quote'):
+            yield{
+                'quote': quotes.css('.text::text').get(),
+                'author': quotes.css('.author::text').get()
+            }
+        link = response.css('.next a::attr(href)').get()
+        next_link = response.urljoin(link)
+        if next_link:
+            yield SplashRequest(url=next_link,
+            callback=self.parse,
+            endpoint='execute',
+            headers={
+                'User-Agent': self.user_agent
+            },
+            args={
+                'lua_source': self.script
+            })
