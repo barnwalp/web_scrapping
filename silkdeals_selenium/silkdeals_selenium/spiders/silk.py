2558cfb9db7fd6dd7f94dbcac2608463541f4f4e
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-06-02 10:14:17 +0530

    feat/scrapping new page using selenium

--- silkdeals_selenium/silkdeals_selenium/spiders/silk.py
+++ silkdeals_selenium/silkdeals_selenium/spiders/silk.py
@@ -1,5 +1,7 @@
 import scrapy
 from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.keys import Keys
+from scrapy.selector import Selector
 
 
 class SilkSpider(scrapy.Spider):
@@ -14,8 +16,26 @@ class SilkSpider(scrapy.Spider):
         )
 
     def parse(self, response):
-        img = response.request.meta['screenshot']
-        # print(response.request.meta['driver'].title)
+        # img = response.request.meta['screenshot']
+        # # int(response.request.meta['driver'].title)
 
-        with open('screenshot.png', 'wb') as f:
-            f.write(img)
+        # with open('screenshot.png', 'wb') as f:
+        #     f.write(img)
+        # Getting the driver instance in the parse method
+        driver = response.request.meta['driver']
+        search_input = driver.find_element_by_xpath("//input[@id='search_form_input_homepage']")
+        search_input.send_keys('Hello World')
+        
+        search_input.send_keys(Keys.ENTER)
+        # saving screenshot
+        # driver.save_screenshot('after_filling.png')
+
+        # convert the current html page to a selector
+        html = driver.page_source
+        response_obj = Selector(text=html)
+
+        for link in response_obj.xpath("//div[@class='result__extras__url']/a"):
+            yield{
+                'URL': link.xpath('.//@href').get()
+            }
+
