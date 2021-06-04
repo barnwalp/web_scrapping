957aa4a6808d1e15480f1c396eeea59d723c50f7
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-06-04 09:18:20 +0530

    feat/implemented all 4 basic pipeline methods

--- silkdeals_selenium/silkdeals_selenium/pipelines.py
+++ silkdeals_selenium/silkdeals_selenium/pipelines.py
@@ -6,8 +6,25 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import logging
 
 
 class SilkdealsSeleniumPipeline:
+    # Purpose of this method is to grab whatever value you want
+    # from settings.py file
+    @classmethod
+    def from_crawler(cls, crawler):
+        # print the value of MONGO_URI defined in settings.py
+        logging.warning(crawler.settings.get('MONGO_URI'))
+
+    # It will be called when spider starts execution process
+    def open_spider(self, spider):
+        logging.warning("SPIDER OPENED FROM PIPELINE")
+    
+    # It will be called when spider finishes execution
+    def close_spider(self, spider):
+        logging.warning("SPIDER CLOSED FROM PIPELINE")
+
+    # For each item scrapped, process_item method is called
     def process_item(self, item, spider):
         return item
