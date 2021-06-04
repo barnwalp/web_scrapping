5eb25b2fc42094f0da4dde7bc81635576e8bdfaa
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-06-04 07:13:41 +0530

    feat/normalized whitespaces

--- silkdeals_selenium/silkdeals_selenium/spiders/computer_deals.py
+++ silkdeals_selenium/silkdeals_selenium/spiders/computer_deals.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy_selenium import SeleniumRequest
 from selenium.webdriver.common.keys import Keys
 from scrapy.selector import Selector
+import re
 
 
 class ComputerDealsSpider(scrapy.Spider):
@@ -19,5 +20,18 @@ class ComputerDealsSpider(scrapy.Spider):
         for link in response.css('.fpItem'):
             yield{
                 'product_name': link.css('.itemImageLink a::text').get(),
-                'product_price': link.css('.priceLine .itemPrice::text').get()
+                'product_link': link.css('.itemImageLink a::attr(href)').get(),
+                'product_price': self.normalize_whitespace(link.css('.priceLine .itemPrice::text').get()),
+                'store_name': link.css('.itemStore.bp-p-storeLink::text').get()
             }
+
+    def normalize_whitespace(self, str):
+        # removing whitespace from front and tail of string
+        str = str.strip()
+        # removing unicode chanracter from the strings
+        str = str.encode("ascii", "ignore").decode()
+        # re.sub replace occurence of particular substring with another
+        # substring. In this case occurence of 1 or more whitespce in between
+        # words are replaced with one whitespace
+        str = re.sub(r'\s+', ' ', str)
+        return str
