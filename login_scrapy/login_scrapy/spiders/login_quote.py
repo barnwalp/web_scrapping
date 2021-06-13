e37454621fad15b3d927b509dfdd943e85f046ee
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-06-13 09:50:27 +0530

    feat/login using scrapy and api

---
+++ login_scrapy/login_scrapy/spiders/login_quote.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy import FormRequest
+
+
+class LoginQuoteSpider(scrapy.Spider):
+    name = 'login_quote'
+    start_urls = ['http://quotes.toscrape.com/login']
+
+    def parse(self, response):
+        csrf_token = response.css('input[name="csrf_token"]::attr(value)').get()
+        yield FormRequest.from_response(
+            response,
+            formcss='form',
+            formdata={
+                'csrf_token': csrf_token,
+                'username': 'admin',
+                'password': 'admin',
+            },
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        if response.css('a[href="/logout"]::text').get():
+            print('login successful')
